aca40a15e0d2322cd7a581a3ce64def4bc1ffb98
Eric D. Helms <ericdhelms@gmail.com>
2018-02-02 13:46:46 -0500

    Re-organize tooling playbooks for containers

--- containers/roles/candlepin/files/entrypoint.sh
+++ containers/roles/candlepin/files/entrypoint.sh
@@ -7,12 +7,12 @@ set -x
 /usr/bin/gen-certs
 
 /usr/share/candlepin/cpdb --create \
-                          --schema-only \
-                          --dbhost=$POSTGRES_SERVICE \
-                          --dbport=$POSTGRES_PORT \
-                          --database=$POSTGRES_DB \
-                          --user=$POSTGRES_USER  \
-                          --password=$POSTGRES_PASSWORD
+										      --schema-only \
+										      --dbhost=$POSTGRES_SERVICE \
+										      --dbport=$POSTGRES_PORT \
+										      --database=$POSTGRES_DB \
+										      --user=$POSTGRES_USER  \
+										      --password=$POSTGRES_PASSWORD
 
 qpid-config -b tcp://${QPID_SERVICE}:${QPID_PORT} exchanges event
 
