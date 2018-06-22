0b171ca63eb2f93d996325e3b6c861717c22e6bc
Eric D. Helms <ericdhelms@gmail.com>
2018-06-22 15:09:19 -0400

    Re-factor Candlepin containers

--- containers/images/candlepin/container-assets/start_candlepin.sh
+++ containers/images/candlepin/container-assets/start_candlepin.sh
@@ -2,4 +2,7 @@
 
 set -x
 
+# Just start tomcat instead of all of this madness  /usr/share/tomcat/bin/start
+# ExecStart=/usr/libexec/tomcat/server start
+
 java -classpath /usr/share/tomcat/bin/bootstrap.jar:/usr/share/tomcat/bin/tomcat-juli.jar:/usr/share/java/commons-daemon.jar -Dcatalina.base=/usr/share/tomcat -Dcatalina.home=/usr/share/tomcat -Djava.endorsed.dirs= -Djava.io.tmpdir=/var/cache/tomcat/temp -Djava.util.logging.config.file=/usr/share/tomcat/conf/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager org.apache.catalina.startup.Bootstrap start
